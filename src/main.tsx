76cfee2926dd9225e4a0e0c5699dfa5bce50e215
kknguye <kknguye@clemson.edu>
2025-11-05 00:01:29 -0500

    Fix router

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <App /> },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
